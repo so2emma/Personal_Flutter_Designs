d0e5f0a07c576fea6c40849f70393ad4d5105609
so2emma <osoemmanuel1969@gmail.com>
2024-03-05 23:47:17 +0100

    created the bottom Nav

--- plant_app/lib/ui/root_page.dart
+++ plant_app/lib/ui/root_page.dart
@@ -1,9 +1,13 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:plant_app/constants.dart';
+import 'package:plant_app/ui/login_page.dart';
 import 'package:plant_app/ui/screens/cart_page.dart';
 import 'package:plant_app/ui/screens/favourite_page.dart';
 import 'package:plant_app/ui/screens/home_page.dart';
 import 'package:plant_app/ui/screens/profile_page.dart';
+import 'package:plant_app/ui/screens/scan_page.dart';
 
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
@@ -29,7 +33,7 @@ class _RootPageState extends State<RootPage> {
     Icons.person
   ];
 
-  List<String> titleList = ['Home', 'Favourite', 'Cart', 'profile'];
+  List<String> titleList = ['Home', 'Favourite', 'Cart', 'Profile'];
 
   @override
   Widget build(BuildContext context) {
@@ -44,9 +48,46 @@ class _RootPageState extends State<RootPage> {
                   color: Constants.blackColor,
                   fontWeight: FontWeight.w500,
                   fontSize: 24),
-            )
+            ),
+            Icon(
+              Icons.notifications,
+              color: Constants.blackColor,
+              size: 30.0,
+            ),
           ],
         ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0.0,
+      ),
+      body: IndexedStack(
+        index: _bottomNavIndex,
+        children: pages,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              PageTransition(
+                  child: const ScanPage(),
+                  type: PageTransitionType.bottomToTop));
+        },
+        child: Image.asset('assets/images/code-scan.png', height: 30.0),
+        backgroundColor: Constants.primaryColor,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        splashColor: Constants.primaryColor,
+        activeColor: Constants.primaryColor,
+        inactiveColor: Colors.black.withOpacity(.5),
+        icons: iconList,
+        activeIndex: _bottomNavIndex,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.softEdge,
+        onTap: (index) {
+          setState(() {
+            _bottomNavIndex = index;
+          });
+        },
       ),
     );
   }
