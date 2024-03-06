e1e37a7d37b4fad878cb6bfa5ff7633dbc3c7337
so2emma <osoemmanuel1969@gmail.com>
2024-03-06 11:13:13 +0100

    redesigned the favourite page.

--- plant_app/lib/ui/screens/favourite_page.dart
+++ plant_app/lib/ui/screens/favourite_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
 import 'package:plant_app/models/plants.dart';
+import 'package:plant_app/ui/screens/widget/plant_widget.dart';
 
 class FavouritePage extends StatefulWidget {
   final List<Plant> favoritePlants;
@@ -12,10 +14,43 @@ class FavouritePage extends StatefulWidget {
 class _FavouritePageState extends State<FavouritePage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text('Favourite Page'),
-      ),
-    );
+        body: widget.favoritePlants.isEmpty
+            ? Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 100,
+                      child: Image.asset('assets/images/favorited.png'),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Your Favorited Plants',
+                      style: TextStyle(
+                          color: Constants.primaryColor,
+                          fontWeight: FontWeight.w300,
+                          fontSize: 18),
+                    )
+                  ],
+                ),
+              )
+            : Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 30),
+                height: size.height * .5,
+                child: ListView.builder(
+                  itemBuilder: (BuildContext context, int index) {
+                    return PlantWidget(
+                        index: index, plantList: widget.favoritePlants);
+                  },
+                  itemCount: widget.favoritePlants.length,
+                  scrollDirection: Axis.vertical,
+                  physics: BouncingScrollPhysics(),
+                ),
+              ));
   }
 }
--- plant_app/lib/ui/screens/home_page.dart
+++ plant_app/lib/ui/screens/home_page.dart
@@ -3,6 +3,7 @@ import 'package:page_transition/page_transition.dart';
 import 'package:plant_app/constants.dart';
 import 'package:plant_app/models/plants.dart';
 import 'package:plant_app/ui/screens/detail_page.dart';
+import 'package:plant_app/ui/screens/widget/plant_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -233,81 +234,9 @@ class _HomePageState extends State<HomePage> {
                 scrollDirection: Axis.vertical,
                 physics: BouncingScrollPhysics(),
                 itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          PageTransition(
-                              child: DetailPage(
-                                plantId: index,
-                              ),
-                              type: PageTransitionType.bottomToTop));
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Constants.primaryColor.withOpacity(.1),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      height: 80.0,
-                      padding: EdgeInsets.only(left: 10, top: 10),
-                      margin: EdgeInsets.only(bottom: 10.0, top: 10),
-                      width: size.width,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Stack(
-                            clipBehavior: Clip.none,
-                            children: [
-                              Container(
-                                width: 60.0,
-                                height: 60.0,
-                                decoration: BoxDecoration(
-                                  color: Constants.primaryColor.withOpacity(.8),
-                                  shape: BoxShape.circle,
-                                ),
-                              ),
-                              Positioned(
-                                  bottom: 5,
-                                  left: 0,
-                                  right: 0,
-                                  child: SizedBox(
-                                    height: 80,
-                                    child:
-                                        Image.asset(_plantList[index].imageURL),
-                                  )),
-                              Positioned(
-                                  bottom: 5,
-                                  left: 80,
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(_plantList[index].category),
-                                      Text(
-                                        _plantList[index].plantName,
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 18,
-                                            color: Constants.blackColor),
-                                      )
-                                    ],
-                                  ))
-                            ],
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(right: 10),
-                            child: Text(
-                              r'$' + _plantList[index].price.toString(),
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 18,
-                                  color: Constants.primaryColor),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
+                  return PlantWidget(
+                    index: index,
+                    plantList: _plantList,
                   );
                 }),
           )
